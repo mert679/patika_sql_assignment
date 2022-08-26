c723ea20791e681d21270bbe1bb4e5cdc6446b3a
Osman Mert Altıntaş <64027475+mert679@users.noreply.github.com>
2022-08-26 16:24:06 +0300

    Create ödev3.sql

---
+++ ödev3.sql
@@ -0,0 +1,4 @@
+select country from country where country like 'A%a'
+select country from country where country like '_____n'
+SELECT title FROM film where title ILIKE '%t%t%t%t%'
+SELECT * from film where title like 'C%' and (length > 90 and rental_rate=2.99)
