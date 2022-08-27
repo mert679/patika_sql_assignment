fb423e270c3269a9bea382245474dc2d9d811bf6
Osman Mert Altıntaş <64027475+mert679@users.noreply.github.com>
2022-08-27 17:24:39 +0300

    Create odev4.sql

---
+++ odev4.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT (replacement_cost) from film; 
+
+select count( distinct replacement_cost) from film;
+select * from film where title LIKE 'T%' and rating='G' 
+select count(country) from country where country like '_____'
+select * from city where city ilike '%r'
