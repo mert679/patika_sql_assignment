d2be87c93fa909f4b6ba51a9f3657519f9dd4000
Osman Mert Altıntaş <64027475+mert679@users.noreply.github.com>
2022-09-04 11:54:01 +0300

    Create odev12.sql

---
+++ odev12.sql
@@ -0,0 +1,12 @@
+select count(*) from film 
+where length >(
+	select avg(length) from film 
+)
+
+SELECT count(*)  from film where rental_rate = (select max(rental_rate) from film)
+
+select * from film where rental_rate = (select min(rental_rate) from film)
+and  replacement_cost = (select min(replacement_cost) from film)
+
+
+select customer_id,sum(amount) from payment group by customer_id order by sum(amount) desc
